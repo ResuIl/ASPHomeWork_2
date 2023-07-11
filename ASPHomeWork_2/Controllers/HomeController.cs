4a95c7ca4265b81525243da89fc965af69af075a
Rəsul <71267982+ResuIl@users.noreply.github.com>
2023-07-12 01:47:16 +0400

    Add project files.

---
+++ ASPHomeWork_2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ASPHomeWork_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace ASPHomeWork_2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Content()
+    {
+        return Content("SALAM DUNYA");
+    }
+
+    public ViewResult URLQueryTest(TestController fm)
+    {
+        return View(fm);
+    }
+
+    public JsonResult Json()
+    {
+        var json = JsonSerializer.Serialize("{Id: 1, Name: Resul, Surname: Black, Age: 31}");
+        return Json(json);
+    }
+}
---
+++ ASPHomeWork_2/Models/TestController.cs
@@ -0,0 +1,8 @@
+﻿namespace ASPHomeWork_2.Models
+{
+    public class TestController
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
